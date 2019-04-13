279db1fa47f140fb77f96f25fc87f9ff5a6e25ea
jaythemishra <jaythemishra@gmail.com>
2019-04-12 18:56:19 -0700

    Passed all 3 Test Cases

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -2,13 +2,12 @@ package mapreduce
 
 import "container/list"
 import "fmt"
-
+import "sync"
 
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -28,7 +27,41 @@ func (mr *MapReduce) KillWorkers() *list.List {
 	return l
 }
 
+//RunMaster assigns Map and Reduce jobs to available workers
 func (mr *MapReduce) RunMaster() *list.List {
-	// Your code here
+	AssignJob := func(jobNumber int, operation string, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for {
+			worker := <-mr.registerChannel
+			args := &DoJobArgs{}
+			args.File = mr.file
+			args.Operation = JobType(operation)
+			args.JobNumber = jobNumber
+			if operation == Map {
+				args.NumOtherPhase = mr.nReduce
+			} else if operation == Reduce {
+				args.NumOtherPhase = mr.nMap
+			}
+			var reply DoJobReply
+			ok := call(worker, "Worker.DoJob", args, &reply)
+			if ok == true {
+				go func() {
+					mr.registerChannel <- worker
+				}()
+				break
+			}
+		}
+	}
+	var wg sync.WaitGroup
+	for i := 0; i < mr.nMap; i++ {
+		wg.Add(1)
+		go AssignJob(i, Map, &wg)
+	}
+	wg.Wait()
+	for i := 0; i < mr.nReduce; i++ {
+		wg.Add(1)
+		go AssignJob(i, Reduce, &wg)
+	}
+	wg.Wait()
 	return mr.KillWorkers()
 }
